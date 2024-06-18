cccd3c26ca8304a3997ce5ada60ccc60a6c037ea
hocinedev <hocinedev4@gmail.com>
2024-06-18 11:32:09 +0100

    register form validation

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Length,EqualTo,Email,DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
-    email= StringField(label='Email Adresse:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Confirme Password:')
+    username = StringField(label='User Name:',validators=[Length(min=4,max=30),DataRequired()])
+    email= StringField(label='Email Adresse:',validators=[Email(),DataRequired()])
+    password1 = PasswordField(label='Password:',validators=[Length(min=6,max=30),DataRequired()])
+    password2 = PasswordField(label='Confirm Password:',validators=[EqualTo('password1',message='Password and confirm password mismatch.'),DataRequired()])
     submit=SubmitField(label='Create Account')
--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from market import app,db
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash
 from market.models import Item,User
 from market.forms import RegisterForm
 @app.route('/')
@@ -16,14 +16,20 @@ def Market():
 @app.route('/login')
 def Login():
     return render_template('index.html')
-@app.route('/register',methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def Register():
     form = RegisterForm()
     if form.validate_on_submit():
-        user = User(username = form.username.data,
+        user = User(username=form.username.data,
                     email=form.email.data,
                     password_hash=form.password1.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('Market'))
+    
+    if form.errors:
+        for err_msgs in form.errors.values():
+            for err_msg in err_msgs:
+                flash(f'{err_msg}', 'danger')
+    
     return render_template('register.html', form=form)
