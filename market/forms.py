6fd6ea3ac4aa0a73ccc567e5ef4c6c8559775e17
hocinedev <hocinedev4@gmail.com>
2024-06-19 19:47:21 +0100

    register page improve

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Length,EqualTo,Email,DataRequired
+from wtforms.validators import Length,EqualTo,Email,DataRequired,ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
+    def validate_username(self,username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if(user):
+            raise ValidationError('Username already exist, please try another username')
+    def validate_email(self,email_to_check):
+        user = User.query.filter_by(email=email_to_check.data).first()
+        if(user):
+            raise ValidationError('Email already exist, please try another username')   
+        
     username = StringField(label='User Name:',validators=[Length(min=4,max=30),DataRequired()])
     email= StringField(label='Email Adresse:',validators=[Email(),DataRequired()])
     password1 = PasswordField(label='Password:',validators=[Length(min=6,max=30),DataRequired()])
